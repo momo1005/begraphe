dab3cfb8eef9e0bb29da4aacf497cb8ec4bb3839
momo1005 <morgan.yakhelef@hotmail.com>
2021-05-03 11:31:18 +0200

    Dijkstra v1

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,15 +1,123 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.insa.graphs.algorithm.AbstractSolution.Status;
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Path;
+import org.insa.graphs.model.Label;
+
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
+    
     public DijkstraAlgorithm(ShortestPathData data) {
         super(data);
     }
+    
 
     @Override
-    protected ShortestPathSolution doRun() {
+    protected ShortestPathSolution doRun() {        
+     // On récupere le graphe
         final ShortestPathData data = getInputData();
+        Graph graph = data.getGraph();
+        
+        //Nombre de noeud dans le graph
+        final int nbNodes = graph.size();
+        
+    	//Associer un label à chaque noeud
+    	//Les noeuds des graphes sont numérotés de 0 à N-1.
+    	Label[] labels = new Label[nbNodes];
+
+        //On créer et associe les labels à chaque noeud
+    	//dans label on a la node
+    	//et sommet_courant = id de la node = indice du tableau
+    	//les label sont rangé selon leurs id -> a l'indice de l'id boom on a le label associé
+    	//id du node < nombre de node (normalemenrt c'est forcément vrai)
+        for (Node node: graph.getNodes()) {
+        	labels[node.getId()]=new Label(node);
+        }
+        
+        //On met a jour pour l'origine - Le sommet est marqué avec un cout de 0
+        labels[data.getOrigin().getId()].setMarqued(true);
+        labels[data.getOrigin().getId()].setCost(0);
+
+        
+        //On creer un tas binaire sur les labels/etiquettes du code
+        //Car on va trier en fonction du min
+        //or si on met les noeud on ne va pas avoir 
+        BinaryHeap<Label> tas_binaire = new BinaryHeap<Label>();
+        tas_binaire.insert(labels[data.getOrigin().getId()]);
+        
+        
+        boolean pas_fini = true;
+        for(Label lab : labels) {
+        	if (lab.getMarqued()==false) {
+        		pas_fini=false;
+        	}
+        }
+    
+        Label x;
+        while(pas_fini==true) {
+        	//On enleve le min du tas
+            x=tas_binaire.deleteMin();
+            //On met a jour marqued de x à true
+            x.setMarqued(true);
+            
+            //on regarde les successeur du noeud associé au label x
+            //Attention ici on a des arcs le succeseur ça va etre successeur[i].getDestination()
+            List<Arc> sucesseurs = x.getNode().getSuccessors(); 
+            
+            for (Arc arc: sucesseurs) {
+            	int indice = arc.getDestination().getId(); //id du node succeseur
+            	//si le succeseur n'est pas marqué
+            	if (!labels[indice].getMarqued()) {
+            		double old_cost=labels[indice].getCost();
+            		double new_cost=labels[x.getSommet_Courant()].getCost()+data.getCost(arc);
+            		double maj_cost=Double.min(old_cost,new_cost); //nouvelle valeur de cost
+            		labels[indice].setCost(maj_cost);
+            		
+            		//Si le cout a été mis a jour
+            		//alors l'ancienne valeur a été remplacé par une plus petite
+            		if(old_cost != maj_cost) {
+            			//ATTENTION :
+            			//old_cost=infini -> il etait pas dans le tas : pas de soucis
+            			//old-cost!=infini alors il etait deja dans le tas 
+            			//Dans ce cas la on utilise remove pour ne pas le mettre 2 fois !
+            			//On remove l'ancienne valeur et on insert la nouvelle(=label mis a jour)
+            			
+            			
+            			//placer le label associé au noeud dans le tas
+            	        tas_binaire.insert(labels[indice]);
+            	        //on met a jour le pere
+            	        labels[indice].setFather(arc); //on met des arc donc la l'arc entre x et son sucesseur
+            		}
+            		
+            	}
+            	
+            	//on met a jour notre variable
+            	for(Label lab : labels) {
+                	if (lab.getMarqued()==false) {
+                		pas_fini=false;
+                	}
+                }
+            	
+            }
+            
+         }
+        
+        
         ShortestPathSolution solution = null;
+        
+     
+        
+        
         // TODO:
         return solution;
     }
--- be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
@@ -1,27 +1,80 @@
 package org.insa.graphs.model;
 
-public class Label {
+//on a besoin d'implementer comparable car on met un compareto
+//On met un compare to pour pouvoir utiliser binaryHeap
+//on fait ça sur les label car dans node le compare tout est sur les id = inutile
+//compareto sur les cout ici !
+public class Label implements Comparable<Label> {
 	//sommet associé à ce label
 	private int sommet_courant;
+	
+	//node associé
+	private Node node;
 	
 	//booléen, vrai lorsque le coût min de ce sommet est définitivement connu par l'algorithme.
 	private boolean marqued;
 	
 	//valeur courante du plus court chemin depuis l'origine vers le sommet.
-	private int cost;
+	private double cost;
 	
 	//correspond au sommet précédent sur le chemin correspondant au plus court chemin courant.
 	private Arc father;
 	
 	//constructeur
-	public Label(int sommet,boolean marque,int cout,Arc pere) {
-        this.sommet_courant=sommet;
-        this.marqued=marque;
+	public Label(Node node) {
+		this.node=node;
+        this.sommet_courant=node.getId();
+        this.marqued=false;
+        this.cost=Double.POSITIVE_INFINITY;
+        this.father=null;
+    }
+	
+	public Node getNode() {
+        return this.node;
+    }
+	
+	public double getCost() {
+        return this.cost;
+    }
+	
+	public int getSommet_Courant() {
+        return this.sommet_courant;
+    }
+	
+	public boolean getMarqued() {
+        return this.marqued;
+    }
+	
+	public Arc getFather() {
+        return this.father;
+    }
+	
+	public void setCost(double cout) {
         this.cost=cout;
+    }
+	
+	public void setSommet_courant(int sommet) {
+        this.sommet_courant=sommet;
+    }
+	
+	public void setMarqued(boolean marque) {
+        this.marqued=marque;
+    }
+	
+	public void setFather(Arc pere) {
         this.father=pere;
     }
 	
-	public int getCost() {
-        return this.cost;
+	/**
+     * Compare the cost of this label with the cost of the given label.
+     * 
+     * @param other lable to compare this label with.
+     * 
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    public int compareTo(Label other) {
+        return Double.compare(getCost(), other.getCost());
     }
+	
+	
 }
