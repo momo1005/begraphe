cfb5ad72a3e98170a5790c0db49b5dc6e9ad8497
momo1005 <morgan.yakhelef@hotmail.com>
2021-05-18 17:33:42 +0200

    Update LabelStar.java

--- be-graphes-model/src/main/java/org/insa/graphs/model/LabelStar.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/LabelStar.java
@@ -2,7 +2,6 @@ package org.insa.graphs.model;
 
 public class LabelStar extends Label {
 	private double cost_estimated;
-	//private double cost_total; //on creer pas de variable on va juste faire le calcul de la somme
 
 	public LabelStar(Node node, double cout_estime) {
 		super(node);
@@ -10,17 +9,17 @@ public class LabelStar extends Label {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void setCost_Estimated(double cout) {
-        this.cost_estimated=cout;
-    }
+//	public void setCost_Estimated(double cout) {
+//        this.cost_estimated=cout;
+//    }
 	
 	public double getCost_Estimated() {
-        return this.cost_estimated;
+		return this.cost_estimated;
     }
 
 	//On redefinit getCost_total
 	public double getCost_total() {
-		return this.getCost()+this.cost_estimated;
+		return super.getCost()+this.cost_estimated;
 	}
 	
 	//on a utilisé la siouxe solution consistant a definir getTotalCost dans Label
@@ -30,6 +29,4 @@ public class LabelStar extends Label {
 //	public int compareTo(LabelStar other) {
 //        return Double.compare(getCost_total(), other.getCost_total());
 //    }
-	
-
 }
