af6b35a77520ef8eff64d25ce7b3134f4ec26b8f
momo1005 <morgan.yakhelef@hotmail.com>
2021-05-24 20:12:51 +0200

    Update DijkstraAlgorithm.java

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -70,7 +70,7 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
             //Notify node has been marqued
             notifyNodeMarked(x.getNode());
             
-            //System.out.println("Cout du noeud n° "+x.getSommet_Courant() +" qui viens d'etre marqué ="+x.getCost()+" cout estimer = "+x.getCost_Estimated()+ " et cout totale = "+x.getCost_total());
+            System.out.println("Cout du noeud n° "+x.getSommet_Courant() +" qui viens d'etre marqué ="+x.getCost()+" cout estimer = "+x.getCost_Estimated()+ " et cout totale = "+x.getCost_total());
             
             //on regarde les successeur du noeud associé au label x
             //Attention ici on a des arcs le succeseur ça va etre successeur[i].getDestination()
@@ -162,7 +162,6 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     }
     
     //INITIALISATION DES LABELS
-    //mettre en parametre ShortestPathData data et creer labels directement dedans pour pas avoir de soucis avec label et labelstar
 
     protected void init(Graph graph,Label[] labels) {
     	for (Node node: graph.getNodes()) {
